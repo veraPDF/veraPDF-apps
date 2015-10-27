184af2faf46e622639b8f3b7fc6fb268af7506d9
Carl Wilson <carl@openplanetsfoundation.org>
2015-10-27 08:20:42 +0000

    Merged GUI and CLI apps.

---
+++ src/main/java/org/verapdf/cli/VeraPdfCli.java
@@ -0,0 +1,77 @@
+/**
+ *
+ */
+package org.verapdf.cli;
+
+import javax.xml.bind.JAXBException;
+
+import org.verapdf.cli.commands.CommandVeraPDF;
+import org.verapdf.config.Input;
+import org.verapdf.config.VeraPdfTaskConfig;
+import org.verapdf.core.VeraPDFException;
+import org.verapdf.pdfa.results.ValidationResult;
+import org.verapdf.runner.ValidationRunner;
+
+import com.beust.jcommander.JCommander;
+
+public class VeraPdfCli {
+
+    private static final CommandVeraPDF commandVeraPDF;
+
+    static {
+        commandVeraPDF = new CommandVeraPDF();
+    }
+
+    private VeraPdfCli() {
+        // disable default constructor
+    }
+
+    /**
+     * Main CLI entry point, process the command line arguments
+     *
+     * @param args
+     *            Java.lang.String array of command line args, to be processed
+     *            using Apache commons CLI.
+     */
+    public static void main(String[] args) {
+        JCommander jCommander = new JCommander();
+        jCommander.addCommand(commandVeraPDF);
+        jCommander.parse(args);
+
+        VeraPdfTaskConfig taskConfig = createConfigFromCliOptions(commandVeraPDF);
+        long startTime = System.currentTimeMillis();
+        ValidationResult validationResult;
+            try {
+                validationResult = ValidationRunner
+                        .runValidation(taskConfig);
+                System.out.println(validationResult.toString());
+            } catch (VeraPDFException e) {
+                e.printStackTrace();
+            }
+    }
+
+    /**
+     * Creates instance of VeraPdfTaskConfig from parsed cli options
+     * 
+     * @param cmdVeraPDF
+     *            options used by VeraPDF software
+     * @return an immutable VeraPdfTaskConfig object populated from the parsed
+     *         options
+     */
+    private static VeraPdfTaskConfig createConfigFromCliOptions(
+            final CommandVeraPDF cmdVeraPDF) {
+        VeraPdfTaskConfig.Builder configBuilder = new VeraPdfTaskConfig.Builder();
+        configBuilder
+                .input(new Input(cmdVeraPDF.getInputPath(), cmdVeraPDF
+                        .isInputPathURL()))
+				.profile(cmdVeraPDF.getProfile())
+                .validate(cmdVeraPDF.isValidate())
+                .output(cmdVeraPDF.getOutput())
+				.maxFailedChecks(cmdVeraPDF.getMaxFailedChecks())
+				.maxDisplayedFailedChecks(cmdVeraPDF.getMaxDisplayedFailedChecks())
+				.logPassedChecks(cmdVeraPDF.isLogPassedChecks())
+				.incrementalSave(cmdVeraPDF.getIncrementalSave());
+        return configBuilder.build();
+    }
+
+}
---
+++ src/main/java/org/verapdf/runner/ValidationRunner.java
@@ -0,0 +1,70 @@
+package org.verapdf.runner;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import org.verapdf.config.VeraPdfTaskConfig;
+import org.verapdf.core.ValidationException;
+import org.verapdf.core.VeraPDFException;
+import org.verapdf.metadata.fixer.MetadataFixer;
+import org.verapdf.metadata.fixer.MetadataFixerResult;
+import org.verapdf.metadata.fixer.impl.pb.FixerConfigImpl;
+import org.verapdf.metadata.fixer.utils.FixerConfig;
+import org.verapdf.model.ModelLoader;
+import org.verapdf.pdfa.flavours.PDFAFlavour;
+import org.verapdf.pdfa.results.ValidationResult;
+import org.verapdf.pdfa.results.ValidationResults;
+import org.verapdf.validation.logic.Validator;
+
+public class ValidationRunner {
+
+    private ValidationRunner() {
+        // Disable default constructor
+    }
+
+    /**
+     * Helper method to run validation
+     * 
+     * @param config
+     *            validation task configuration
+     * @return the validation result
+     * @throws VeraPDFException 
+     */
+    public static ValidationResult runValidation(VeraPdfTaskConfig config) throws VeraPDFException {
+        try (ModelLoader loader = new ModelLoader(config.getInput().getPath())) {
+            org.verapdf.model.baselayer.Object root;
+            try {
+                root = loader.getRoot();
+            } catch (IOException e) {
+                throw new VeraPDFException("IOException when parsing Validation Model.", e);
+            }
+            System.out.println("=================================");
+            ValidationResult result = Validator.validate(PDFAFlavour.PDFA_1_B,
+                    root);
+            ValidationResults.toXml(result, System.out, Boolean.TRUE);
+            if (config.getIncrementalSave() != null
+                    && !config.getIncrementalSave().trim().isEmpty()) {
+                FixerConfig fixerConfig;
+                try {
+                    fixerConfig = FixerConfigImpl.getFixerConfig(loader.getPDDocument(), result);
+                } catch (IOException e) {
+                    throw new VeraPDFException("IOException when parsing Validation Model.", e);
+                }
+                // TODO : what we need do with fixing result?
+                MetadataFixerResult fixerResult =
+                MetadataFixer.fixMetadata(loader.getFile(), fixerConfig);
+            }
+            return result;
+            // TODO: Better exception handling, we need a policy and this isn't
+            // it.
+            // Carl to think a little harder and tidy up, it's not a new idea
+            // I'm after,
+            // more a case of ensuring we use the best of 2 methods.
+        } catch (ValidationException | FileNotFoundException | JAXBException e) {
+            throw new VeraPDFException("Exception when validating.", e);
+        }
+    }
+
+}
