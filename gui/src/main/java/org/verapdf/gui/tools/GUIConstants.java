f387e34bb11e5a01e21adcb9b4facfafea6a6b81
Timur Kamalov <timur.kamalov@duallab.com>
2016-03-31 15:02:16 +0300

    Fixed error message about ecnrypted pdf document in gui.

--- gui/src/main/java/org/verapdf/gui/tools/GUIConstants.java
+++ gui/src/main/java/org/verapdf/gui/tools/GUIConstants.java
@@ -36,6 +36,7 @@ public final class GUIConstants {
 	public static final String CONSORTIUM_TEXT = "© 2015 veraPDF Consortium";
 	public static final String PROPERTIES_NAME = "config.properties";
 	public static final String TITLE = "PDF/A Conformance Checker";
+	public static final String ENCRYPTED_PDF = "Pdf document is encrypted.";
 	public static final String ERROR_IN_PARSING = "Failed to parse pdf document.";
 	public static final String ERROR_IN_VALIDATING = "Internal validation error. The PDF document might be corrupted";
 	public static final String ERROR_IN_INCREMETAL_SAVE = "Some error in saving changes";
