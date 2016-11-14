cfc4a2d7a9313eeb6525c24803f43e610e939d15
Carl Wilson <carl@openplanetsfoundation.org>
2016-11-14 15:19:00 +0000

    FEAT - GUI Batch Processing - fixed profile ordering issue; - added tests for TaskType assignment; - reinstated ProcessorTests; and - GUI options still not all tested.

--- gui/src/main/java/org/verapdf/cli/VeraPdfCliProcessor.java
+++ gui/src/main/java/org/verapdf/cli/VeraPdfCliProcessor.java
@@ -3,20 +3,29 @@
  */
 package org.verapdf.cli;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
 import org.apache.log4j.Logger;
 import org.verapdf.apps.ConfigManager;
 import org.verapdf.apps.VeraAppConfig;
 import org.verapdf.cli.commands.VeraCliArgParser;
 import org.verapdf.core.VeraPDFException;
-import org.verapdf.processor.*;
+import org.verapdf.processor.BatchProcessor;
+import org.verapdf.processor.ItemProcessor;
+import org.verapdf.processor.ProcessorConfig;
+import org.verapdf.processor.ProcessorFactory;
+import org.verapdf.processor.ProcessorResult;
 import org.verapdf.report.ItemDetails;
-
-import javax.xml.bind.JAXBException;
-import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
@@ -68,14 +77,17 @@ final class VeraPdfCliProcessor {
 			processStream(item, System.in);
 		}
 
+		List<File> toProcess = new ArrayList<>();
 		for (String pdfPath : pdfPaths) {
 			File file = new File(pdfPath);
 			if (file.isDirectory()) {
 				this.baseDirectory = file.getAbsolutePath();
 				processDir(file);
-			} else {
-				processFile(file);
+			} else if (checkFileCanBeProcessed(file)) {
+				toProcess.add(file);
 			}
+			if (!toProcess.isEmpty())
+				processFiles(toProcess);
 		}
 	}
 
@@ -94,20 +106,15 @@ final class VeraPdfCliProcessor {
 		}
 	}
 
-	private void processFile(final File pdfFile) {
-		if (checkFileCanBeProcessed(pdfFile)) {
-			try {
-				List<File> files = new ArrayList<>(1);
-				files.add(pdfFile);
-				BatchProcessor processor = ProcessorFactory.fileBatchProcessor(this.processorConfig);
-				ItemDetails fileDetails = ItemDetails.fromFile(pdfFile);
-				OutputStream reportStream = this.getReportStream(fileDetails.getName());
-				processor.process(files, ProcessorFactory.getHandler(appConfig.getFormat(),
-						appConfig.isVerbose(), reportStream));
-			} catch (VeraPDFException e) {
-				System.err.println("Exception raised while processing " + pdfFile.getAbsolutePath());
-				e.printStackTrace();
-			}
+	private void processFiles(final List<File> files) {
+		try {
+			BatchProcessor processor = ProcessorFactory.fileBatchProcessor(this.processorConfig);
+			OutputStream reportStream = this.getReportStream();
+			processor.process(files,
+					ProcessorFactory.getHandler(appConfig.getFormat(), appConfig.isVerbose(), reportStream));
+		} catch (VeraPDFException e) {
+			System.err.println("Exception raised while processing batch");
+			e.printStackTrace();
 		}
 	}
 
@@ -126,10 +133,9 @@ final class VeraPdfCliProcessor {
 		String reportPath = this.appConfig.getReportFile();
 		try {
 			BatchProcessor processor = ProcessorFactory.fileBatchProcessor(this.processorConfig);
-			OutputStream reportStream = reportPath.isEmpty() ? System.out :
-					new FileOutputStream(reportPath);
-			processor.process(dir, true, ProcessorFactory.getHandler(appConfig.getFormat(),
-					appConfig.isVerbose(), reportStream));
+			OutputStream reportStream = reportPath.isEmpty() ? System.out : new FileOutputStream(reportPath);
+			processor.process(dir, true,
+					ProcessorFactory.getHandler(appConfig.getFormat(), appConfig.isVerbose(), reportStream));
 		} catch (IOException e) {
 			throw new VeraPDFException("Error in creating report file " + reportPath, e);
 		}
@@ -140,11 +146,17 @@ final class VeraPdfCliProcessor {
 
 		ProcessorResult result = processor.process(item, toProcess);
 
-		OutputStream outputReportStream = this.getReportStream(item.getName());
+		OutputStream outputReportStream = this.getReportStream();
 		try {
-			ProcessorFactory.resultToXml(result, System.out, true);
-			ProcessorFactory.configToXml(this.processorConfig, System.out, true);
-		} catch (JAXBException excep) {
+			if (result.isValidPdf() && !result.isEncryptedPdf())
+				ProcessorFactory.resultToXml(result, outputReportStream, true);
+			else {
+				String message = (result.isValidPdf()) ? item.getName() + " is and encrypted PDF."
+						: item.getName() + " is not a valid PDF.";
+				outputReportStream.write(message.getBytes());
+			}
+
+		} catch (JAXBException | IOException excep) {
 			// TODO Auto-generated catch block
 			excep.printStackTrace();
 		}
@@ -159,19 +171,8 @@ final class VeraPdfCliProcessor {
 	}
 
 	@SuppressWarnings("resource")
-	private OutputStream getReportStream(final String itemName) {
+	private static OutputStream getReportStream() {
 		OutputStream reportStream = System.out;
-		String reportFileName = this.constructReportPath(itemName);
-		if (!reportFileName.isEmpty()) {
-			try {
-				reportStream = new FileOutputStream(reportFileName, this.appendData);
-				this.isStdOut = false;
-			} catch (FileNotFoundException ex) {
-				LOGGER.warn("Can't open report file:" + itemName, ex);
-				reportStream = System.out;
-				this.isStdOut = true;
-			}
-		}
 		return reportStream;
 	}
 
