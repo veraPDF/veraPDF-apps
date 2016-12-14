83d1d5588963e90a83b8464414ddad57b1373e85
Maksim Bezrukov <bezrukovm95@gmail.com>
2016-12-14 18:05:32 +0300

    Added policy checks logic into the GUI

--- gui/src/main/java/org/verapdf/gui/ValidateWorker.java
+++ gui/src/main/java/org/verapdf/gui/ValidateWorker.java
@@ -1,33 +1,23 @@
 package org.verapdf.gui;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.List;
-
-import javax.swing.JOptionPane;
-import javax.swing.SwingWorker;
-import javax.xml.transform.TransformerException;
-
 import org.apache.log4j.Logger;
 import org.verapdf.apps.ConfigManager;
+import org.verapdf.apps.ProcessType;
 import org.verapdf.apps.VeraAppConfig;
 import org.verapdf.core.VeraPDFException;
 import org.verapdf.gui.tools.GUIConstants;
 import org.verapdf.pdfa.validation.profiles.ValidationProfile;
 import org.verapdf.pdfa.validation.validators.ValidatorConfig;
-import org.verapdf.processor.BatchProcessor;
-import org.verapdf.processor.FormatOption;
-import org.verapdf.processor.ProcessorConfig;
-import org.verapdf.processor.ProcessorFactory;
-import org.verapdf.processor.TaskType;
+import org.verapdf.policy.PolicyChecker;
+import org.verapdf.processor.*;
 import org.verapdf.processor.reports.BatchSummary;
 import org.verapdf.report.HTMLReport;
+
+import javax.swing.*;
+import javax.xml.transform.TransformerException;
+import java.io.*;
+import java.util.EnumSet;
+import java.util.List;
 
 /**
  * Validates PDF in a new thread.
@@ -36,99 +26,118 @@ import org.verapdf.report.HTMLReport;
  */
 class ValidateWorker extends SwingWorker<BatchSummary, Integer> {
 
-	private static final Logger LOGGER = Logger.getLogger(ValidateWorker.class);
+    private static final Logger LOGGER = Logger.getLogger(ValidateWorker.class);
 
-	private static final String ERROR_IN_OPEN_STREAMS = "Can't open stream from PDF file or can't open stream to temporary XML report file";
-	private static final String ERROR_IN_PROCESSING = "Error during the processing";
-	private static final String ERROR_IN_CREATING_TEMP_FILE = "Can't create temporary file for XML report";
-	private static final String ERROR_IN_SAVING_REPORT = "Can't serialize xml report";
+    private static final String ERROR_IN_OPEN_STREAMS = "Can't open stream from PDF file or can't open stream to temporary XML report file";
+    private static final String ERROR_IN_PROCESSING = "Error during the processing";
+    private static final String ERROR_IN_CREATING_TEMP_FILE = "Can't create temporary file for XML report";
+    private static final String ERROR_IN_SAVING_REPORT = "Can't serialize xml report";
 
-	private List<File> pdfs;
-	private ValidationProfile customProfile;
-	private File policy;
-	private CheckerPanel parent;
-	private ConfigManager configManager;
-	private File xmlReport = null;
-	private File htmlReport = null;
-	private BatchSummary batchSummary = null;
+    private List<File> pdfs;
+    private ValidationProfile customProfile;
+    private File policy;
+    private CheckerPanel parent;
+    private ConfigManager configManager;
+    private File xmlReport = null;
+    private File htmlReport = null;
+    private BatchSummary batchSummary = null;
 
-	ValidateWorker(CheckerPanel parent, List<File> pdfs, ConfigManager configManager, ValidationProfile customProfile, File policy) {
-		if (pdfs == null) {
-			throw new IllegalArgumentException("List of pdf files can not be null");
-		}
-		this.parent = parent;
-		this.pdfs = pdfs;
-		this.configManager = configManager;
-		this.customProfile = customProfile;
-		this.policy = policy;
-	}
+    ValidateWorker(CheckerPanel parent, List<File> pdfs, ConfigManager configManager, ValidationProfile customProfile, File policy) {
+        if (pdfs == null) {
+            throw new IllegalArgumentException("List of pdf files can not be null");
+        }
+        this.parent = parent;
+        this.pdfs = pdfs;
+        this.configManager = configManager;
+        this.customProfile = customProfile;
+        this.policy = policy;
+    }
 
-	@Override
-	protected BatchSummary doInBackground() {
-		try {
-			this.xmlReport = File.createTempFile("veraPDF-tempXMLReport", ".xml");
-			this.xmlReport.deleteOnExit();
-			this.htmlReport = null;
-		} catch (IOException e) {
-			LOGGER.error(ERROR_IN_CREATING_TEMP_FILE, e);
-			this.parent.errorInValidatingOccur(ERROR_IN_CREATING_TEMP_FILE + ": ", e);
-		}
-		try (OutputStream mrrReport = new FileOutputStream(this.xmlReport)) {
-			VeraAppConfig veraAppConfig = parent.appConfigFromState();
-			EnumSet<TaskType> tasks = veraAppConfig.getProcessType().getTasks();
-			ValidatorConfig validatorConfig = this.configManager.getValidatorConfig();
-			ProcessorConfig resultConfig = this.customProfile == null
-					? ProcessorFactory.fromValues(validatorConfig,
-					this.configManager.getFeaturesConfig(), this.configManager.getFixerConfig(),
-					tasks, veraAppConfig.getFixesFolder())
-					: ProcessorFactory.fromValues(validatorConfig,
-					this.configManager.getFeaturesConfig(), this.configManager.getFixerConfig(),
-					tasks, customProfile, veraAppConfig.getFixesFolder());
-			BatchProcessor processor = ProcessorFactory.fileBatchProcessor(resultConfig);
-			VeraAppConfig applicationConfig = this.configManager.getApplicationConfig();
-			this.batchSummary = processor.process(this.pdfs,
-						ProcessorFactory.getHandler(FormatOption.MRR, applicationConfig.isVerbose(), mrrReport, applicationConfig.getMaxFailsDisplayed(), validatorConfig.isRecordPasses()));
-		} catch (IOException e) {
-			LOGGER.error(ERROR_IN_OPEN_STREAMS, e);
-			this.parent.errorInValidatingOccur(ERROR_IN_OPEN_STREAMS + ": ", e);
-		} catch (VeraPDFException e) {
-			LOGGER.error(ERROR_IN_PROCESSING, e);
-			this.parent.errorInValidatingOccur(ERROR_IN_PROCESSING + ": ", e);
-		}
+    @Override
+    protected BatchSummary doInBackground() {
+        try {
+            this.xmlReport = File.createTempFile("veraPDF-tempXMLReport", ".xml");
+            this.xmlReport.deleteOnExit();
+            this.htmlReport = null;
+        } catch (IOException e) {
+            LOGGER.error(ERROR_IN_CREATING_TEMP_FILE, e);
+            this.parent.errorInValidatingOccur(ERROR_IN_CREATING_TEMP_FILE + ": ", e);
+        }
+        try (OutputStream mrrReport = new FileOutputStream(this.xmlReport)) {
+            VeraAppConfig veraAppConfig = parent.appConfigFromState();
+            ProcessType processType = veraAppConfig.getProcessType();
+            EnumSet<TaskType> tasks = processType.getTasks();
+            ValidatorConfig validatorConfig = this.configManager.getValidatorConfig();
+            ProcessorConfig resultConfig = this.customProfile == null
+                    ? ProcessorFactory.fromValues(validatorConfig,
+                    this.configManager.getFeaturesConfig(), this.configManager.getFixerConfig(),
+                    tasks, veraAppConfig.getFixesFolder())
+                    : ProcessorFactory.fromValues(validatorConfig,
+                    this.configManager.getFeaturesConfig(), this.configManager.getFixerConfig(),
+                    tasks, customProfile, veraAppConfig.getFixesFolder());
+            BatchProcessor processor = ProcessorFactory.fileBatchProcessor(resultConfig);
+            VeraAppConfig applicationConfig = this.configManager.getApplicationConfig();
+            this.batchSummary = processor.process(this.pdfs,
+                    ProcessorFactory.getHandler(FormatOption.MRR, applicationConfig.isVerbose(), mrrReport, applicationConfig.getMaxFailsDisplayed(), validatorConfig.isRecordPasses()));
 
-		// TODO: change "== 1" to "> 0" when HTML report will work in case of multiply files
-		if (this.batchSummary.getValidPdfaCount() + this.batchSummary.getInvalidPdfaCount() == 1) {
-			writeHtmlReport();
-		}
+            if ((processType == ProcessType.POLICY || processType == ProcessType.POLICY_FIX) && this.policy != null) {
+                applyPolicy();
+            }
+        } catch (IOException e) {
+            LOGGER.error(ERROR_IN_OPEN_STREAMS, e);
+            this.parent.errorInValidatingOccur(ERROR_IN_OPEN_STREAMS + ": ", e);
+        } catch (VeraPDFException e) {
+            LOGGER.error(ERROR_IN_PROCESSING, e);
+            this.parent.errorInValidatingOccur(ERROR_IN_PROCESSING + ": ", e);
+        }
 
-		return this.batchSummary;
-	}
+        // TODO: change "== 1" to "> 0" when HTML report will work in case of multiply files
+        if (this.batchSummary.getValidPdfaCount() + this.batchSummary.getInvalidPdfaCount() == 1) {
+            writeHtmlReport();
+        }
 
-	@Override
-	protected void done() {
-		this.parent.validationEnded(this.xmlReport, this.htmlReport);
-	}
+        return this.batchSummary;
+    }
 
-	private void writeHtmlReport() {
-		try {
-			this.htmlReport = File.createTempFile("veraPDF-tempHTMLReport", ".html");
-			this.htmlReport.deleteOnExit();
-			try (InputStream xmlStream = new FileInputStream(this.xmlReport);
-				 OutputStream htmlStream = new FileOutputStream(this.htmlReport)) {
-				HTMLReport.writeHTMLReport(xmlStream, htmlStream,
-						this.configManager.getApplicationConfig().getWikiPath(), true);
+    private void applyPolicy() throws IOException, VeraPDFException {
+        File tempMrrFile = this.xmlReport;
+        this.xmlReport = File.createTempFile("veraPDF-tempXMLReport", ".xml");
+        this.xmlReport.deleteOnExit();
+        File tempPolicyResult = File.createTempFile("policyResult", "veraPDF");
+        tempPolicyResult.deleteOnExit();
+        try (InputStream mrrIs = new FileInputStream(tempMrrFile);
+             OutputStream policyResultOs = new FileOutputStream(tempPolicyResult);
+             OutputStream mrrReport = new FileOutputStream(this.xmlReport)) {
+            PolicyChecker.applyPolicy(this.policy, mrrIs, policyResultOs);
+            PolicyChecker.insertPolicyReport(tempPolicyResult, tempMrrFile, mrrReport);
+        }
+    }
 
-			} catch (IOException | TransformerException e) {
-				JOptionPane.showMessageDialog(this.parent, GUIConstants.ERROR_IN_SAVING_HTML_REPORT + e.getMessage(),
-						GUIConstants.ERROR, JOptionPane.ERROR_MESSAGE);
-				LOGGER.error("Exception saving the HTML report", e);
-				this.htmlReport = null;
-			}
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(this.parent, GUIConstants.ERROR_IN_SAVING_HTML_REPORT + e.getMessage(),
-					GUIConstants.ERROR, JOptionPane.ERROR_MESSAGE);
-			LOGGER.error("Exception saving the HTML report", e);
-			this.htmlReport = null;
-		}
-	}
+    @Override
+    protected void done() {
+        this.parent.validationEnded(this.xmlReport, this.htmlReport);
+    }
+
+    private void writeHtmlReport() {
+        try {
+            this.htmlReport = File.createTempFile("veraPDF-tempHTMLReport", ".html");
+            this.htmlReport.deleteOnExit();
+            try (InputStream xmlStream = new FileInputStream(this.xmlReport);
+                 OutputStream htmlStream = new FileOutputStream(this.htmlReport)) {
+                HTMLReport.writeHTMLReport(xmlStream, htmlStream,
+                        this.configManager.getApplicationConfig().getWikiPath(), true);
+
+            } catch (IOException | TransformerException e) {
+                JOptionPane.showMessageDialog(this.parent, GUIConstants.ERROR_IN_SAVING_HTML_REPORT + e.getMessage(),
+                        GUIConstants.ERROR, JOptionPane.ERROR_MESSAGE);
+                LOGGER.error("Exception saving the HTML report", e);
+                this.htmlReport = null;
+            }
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(this.parent, GUIConstants.ERROR_IN_SAVING_HTML_REPORT + e.getMessage(),
+                    GUIConstants.ERROR, JOptionPane.ERROR_MESSAGE);
+            LOGGER.error("Exception saving the HTML report", e);
+            this.htmlReport = null;
+        }
+    }
 }
