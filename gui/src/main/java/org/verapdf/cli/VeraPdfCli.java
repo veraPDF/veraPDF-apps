1ed7401eefc58716e973b7697cbaa3cc1a8a6f7b
Carl Wilson <carl@openplanetsfoundation.org>
2017-05-26 10:03:10 +0100

    FEAT - String formatting niceness.

--- gui/src/main/java/org/verapdf/cli/VeraPdfCli.java
+++ gui/src/main/java/org/verapdf/cli/VeraPdfCli.java
@@ -1,22 +1,16 @@
 /**
  * This file is part of VeraPDF Library GUI, a module of the veraPDF project.
- * Copyright (c) 2015, veraPDF Consortium <info@verapdf.org>
- * All rights reserved.
- *
- * VeraPDF Library GUI is free software: you can redistribute it and/or modify
- * it under the terms of either:
- *
- * The GNU General public license GPLv3+.
- * You should have received a copy of the GNU General Public License
- * along with VeraPDF Library GUI as the LICENSE.GPL file in the root of the source
- * tree.  If not, see http://www.gnu.org/licenses/ or
- * https://www.gnu.org/licenses/gpl-3.0.en.html.
- *
- * The Mozilla Public License MPLv2+.
- * You should have received a copy of the Mozilla Public License along with
- * VeraPDF Library GUI as the LICENSE.MPL file in the root of the source tree.
- * If a copy of the MPL was not distributed with this file, you can obtain one at
- * http://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2015, veraPDF Consortium <info@verapdf.org> All rights
+ * reserved. VeraPDF Library GUI is free software: you can redistribute it
+ * and/or modify it under the terms of either: The GNU General public license
+ * GPLv3+. You should have received a copy of the GNU General Public License
+ * along with VeraPDF Library GUI as the LICENSE.GPL file in the root of the
+ * source tree. If not, see http://www.gnu.org/licenses/ or
+ * https://www.gnu.org/licenses/gpl-3.0.en.html. The Mozilla Public License
+ * MPLv2+. You should have received a copy of the Mozilla Public License along
+ * with VeraPDF Library GUI as the LICENSE.MPL file in the root of the source
+ * tree. If a copy of the MPL was not distributed with this file, you can obtain
+ * one at http://mozilla.org/MPL/2.0/.
  */
 /**
  *
@@ -33,6 +27,7 @@ import java.util.logging.Logger;
 import org.verapdf.ReleaseDetails;
 import org.verapdf.apps.Applications;
 import org.verapdf.apps.ConfigManager;
+import org.verapdf.apps.SoftwareUpdater;
 import org.verapdf.cli.commands.VeraCliArgParser;
 import org.verapdf.core.VeraPDFException;
 import org.verapdf.pdfa.PdfBoxFoundryProvider;
@@ -79,12 +74,12 @@ public final class VeraPdfCli {
 			jCommander.parse(args);
 		} catch (ParameterException e) {
 			System.err.println(e.getMessage());
-			showVersionInfo();
+			showVersionInfo(cliArgParser.isVerbose());
 			jCommander.usage();
 			System.exit(1);
 		}
 		if (cliArgParser.isHelp()) {
-			showVersionInfo();
+			showVersionInfo(cliArgParser.isVerbose());
 			jCommander.usage();
 			System.exit(0);
 		}
@@ -95,23 +90,24 @@ public final class VeraPdfCli {
 						configManager);
 				if (args.length == 0)
 					jCommander.usage();
-				// FIXME: trap policy IO Exception (deliberately left un-caught for development)
+				// FIXME: trap policy IO Exception (deliberately left un-caught
+				// for development)
 				processor.processPaths(cliArgParser.getPdfPaths());
 			} catch (OutOfMemoryError oome) {
-				final String message = "The JVM appears to have run out of memory";
+				final String message = "The JVM appears to have run out of memory"; //$NON-NLS-1$
 				logger.log(Level.WARNING, message, oome);
 				MemoryUsage heapUsage = memoryMan.getHeapMemoryUsage();
 				long maxMemory = heapUsage.getMax() / MEGABYTE;
 				long usedMemory = heapUsage.getUsed() / MEGABYTE;
-				System.out.println(message);
-				System.out.println("Memory Use: " + usedMemory + "M/" + maxMemory + "M");
-				System.out.println(
-						"To increase the memory available to the JVM please assign the JAVA_OPTS environment variable.");
-				System.out.println("The examples below increase the maximum heap available to the JVM to 2GB:");
-				System.out.println(" - Mac or Linux users: ");
-				System.out.println("   export JAVA_OPTS=\"-Xmx2048m\"");
-				System.out.println(" - Windows users: ");
-				System.out.println("   SET JAVA_OPTS=\"-Xmx2048m\"");
+				System.out.format(",%s\n", message); //$NON-NLS-1$
+				System.out.format("Memory Use: %sM/%sM\n", Long.valueOf(usedMemory), Long.valueOf(maxMemory)); //$NON-NLS-1$
+				System.out.format(
+						"To increase the memory available to the JVM please assign the JAVA_OPTS environment variable.\n"); //$NON-NLS-1$
+				System.out.format("The examples below increase the maximum heap available to the JVM to 2GB:\n"); //$NON-NLS-1$
+				System.out.format(" - Mac or Linux users:\n"); //$NON-NLS-1$
+				System.out.format("   export JAVA_OPTS=\"-Xmx2048m\"\n"); //$NON-NLS-1$
+				System.out.format(" - Windows users:\n"); //$NON-NLS-1$
+				System.out.format("   SET JAVA_OPTS=\"-Xmx2048m\"\n"); //$NON-NLS-1$
 				System.exit(1);
 			}
 		}
@@ -124,7 +120,7 @@ public final class VeraPdfCli {
 		}
 
 		if (parser.showVersion()) {
-			showVersionInfo();
+			showVersionInfo(parser.isVerbose());
 		}
 	}
 
@@ -133,17 +129,35 @@ public final class VeraPdfCli {
 		EnumSet<PDFAFlavour> flavs = EnumSet.copyOf(PROFILES.getPDFAFlavours());
 		for (PDFAFlavour flav : flavs) {
 			ValidationProfile profile = PROFILES.getValidationProfileByFlavour(flav);
-			System.out.println("  " + profile.getPDFAFlavour().getId() + " - " + profile.getDetails().getName());  //$NON-NLS-1$//$NON-NLS-2$
+			System.out.format("  %s - %s", profile.getPDFAFlavour().getId(), profile.getDetails().getName()); //$NON-NLS-1$
 		}
 		System.out.println();
 	}
 
-	private static void showVersionInfo() {
+	private static void showVersionInfo(final boolean isVerbose) {
 		ReleaseDetails details = ReleaseDetails.byId("gui"); //$NON-NLS-1$
-		System.out.println(APP_NAME + " " + details.getVersion()); //$NON-NLS-1$
-		System.out.println("Built: " + details.getBuildDate()); //$NON-NLS-1$
-		System.out.println(ReleaseDetails.rightsStatement());
-		System.out.println();
+		System.out.format("%s %s\n", APP_NAME, details.getVersion()); //$NON-NLS-1$
+		System.out.format("Built: %s\n", details.getBuildDate()); //$NON-NLS-1$
+		System.out.format("%s\n", ReleaseDetails.rightsStatement()); //$NON-NLS-1$
+		if (isVerbose)
+			showUpdateInfo(details);
+	}
+
+	private static void showUpdateInfo(final ReleaseDetails details) {
+		SoftwareUpdater updater = Applications.softwareUpdater();
+		if (!updater.isOnline()) {
+			logger.log(Level.WARNING, "Update Service not online"); //$NON-NLS-1$
+			return;
+		}
+		if (!updater.isUpdateAvailable(details)) {
+			System.out.format("You are currently running the latest version of veraPDF, v%s\n", details.getVersion()); //$NON-NLS-1$
+			return;
+		}
+		System.out.format(
+				"You are NOT running the latest version of veraPDF.\nYou are running version %s, the latest version is %s.\n", //$NON-NLS-1$
+				details.getVersion(), updater.getLatestVersion(details));
+		System.out.format("You can download the latest version from: %s.\n", //$NON-NLS-1$
+				"http://downloads.verapdf.org/rel/verapdf-installer.zip"); //$NON-NLS-1$
 	}
 
 	private static boolean isProcess(final VeraCliArgParser parser) {
