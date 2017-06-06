84c4622c4ae9a458822e6549631941af7f2f5db5
Carl Wilson <carl@openpreservation.org>
2017-06-06 03:06:52 +0100

    FIX - Updater tests.

--- gui/src/test/java/org/verapdf/apps/SoftwareUpdaterTest.java
+++ gui/src/test/java/org/verapdf/apps/SoftwareUpdaterTest.java
@@ -36,34 +36,4 @@ public class SoftwareUpdaterTest {
 		current = Versions.fromInts(1000, 0, 0);
 		assertTrue(current.compareTo(available) > 0);
 	}
-
-	/**
-	 * Test method for {@link org.verapdf.apps.SoftwareUpdaterImpl#getLatestPdfBoxAppsVersion()}.
-	 */
-	@Test
-	public final void testGetLatestPdfBoxAppsVersion() {
-		if (!this.updater.isOnline()) {
-			System.out.println("Updater is offline");
-			return;
-		}
-		SemanticVersionNumber available = this.updater.getLatestPdfBoxAppsVersion();
-		SemanticVersionNumber current = Versions.fromString("v0.0.1-PDFBOX"); //$NON-NLS-1$
-		assertTrue(current.compareTo(available) < 0);
-		current = Versions.fromString("v10000.0.1-PDFBOX"); //$NON-NLS-1$
-		assertTrue(current.compareTo(available) > 0);
-	}
-
-	/**
-	 * Test method for {@link org.verapdf.apps.SoftwareUpdaterImpl#isUpdateAvailable(java.lang.String)}.
-	 */
-	@Test
-	public final void testIsUpdateAvailableString() {
-		if (!this.updater.isOnline()) {
-			System.out.println("Updater is offline");
-			return;
-		}
-		assertTrue(this.updater.isUpdateAvailable("1.0.0-PDFBOX")); //$NON-NLS-1$
-		assertFalse(this.updater.isUpdateAvailable("200.0.0")); //$NON-NLS-1$
-		assertFalse(this.updater.isUpdateAvailable("150.0.0-PDFBOX")); //$NON-NLS-1$
-	}
 }
