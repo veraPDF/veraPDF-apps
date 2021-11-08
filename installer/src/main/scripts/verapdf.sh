5ad644aeb8a18dfefd78f5084d8037b4dfb2f307
irinamavrina <irina.mavrina@duallab.com>
2021-11-08 17:47:55 +0300

    Added export pkcs package option in scripts

--- installer/src/main/scripts/verapdf.sh
+++ installer/src/main/scripts/verapdf.sh
@@ -127,5 +127,6 @@ exec "$JAVACMD" $JAVA_OPTS  \
   -Dapp.repo="$REPO" \
   -Dapp.home="$BASEDIR" \
   -Dbasedir="$BASEDIR" \
+  --add-exports java.base/sun.security.pkcs=ALL-UNNAMED \
   @verapdf.wrapper.cli@ \
   "$@"
