7d9b5c3f709846ab83f86ca1a538b24eac2d3f72
Maxim <maxplusim@mail.ru>
2026-05-29 16:43:19 +0300

    Update link in SoftwareUpdaterImpl

--- apps/src/main/java/org/verapdf/apps/SoftwareUpdaterImpl.java
+++ apps/src/main/java/org/verapdf/apps/SoftwareUpdaterImpl.java
@@ -50,7 +50,7 @@ import org.xml.sax.InputSource;
 
 public class SoftwareUpdaterImpl implements SoftwareUpdater {
 	private static final Logger logger = Logger.getLogger(SoftwareUpdaterImpl.class.getCanonicalName());
-	private static final String latestGF = "https://search.maven.org/solrsearch/select?q=g:org.verapdf.apps+AND+a:apps&core=gav&rows=1&wt=xml";
+	private static final String latestGF = "https://search.maven.org/solrsearch/select?q=g:org.verapdf.apps+AND+a:gui&core=gav&rows=1&wt=xml";
 	private final String currentVersion = Applications.getAppDetails().getVersion();
 
 	/**
